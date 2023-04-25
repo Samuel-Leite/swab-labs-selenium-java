1bc673e3c93d58ddbdca188c53dd6235839bd464
Samuel Leite <slle@gft.com>
2023-04-25 08:56:43 -0300

    Refatoração do código incluindo pages objects

--- src/main/java/pageObjects/PagamentoPage.java
+++ src/main/java/pageObjects/PagamentoPage.java
@@ -1,5 +1,6 @@
 package pageObjects;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -17,6 +18,10 @@ public class PagamentoPage {
     By txtLastName = By.xpath("//input[@id = 'last-name']");
     By txtPostalCode = By.xpath("//input[@id = 'postal-code']");
     By btnContinue = By.id("continue");
+    By lblProduto = By.xpath("//*[@class='inventory_item_name']");
+    By lblPagamento = By.xpath("//*[@class='summary_value_label']");
+    By btnFinish = By.id("finish");
+    By lblPagamentoSucesso = By.xpath("//*[@class='complete-header']");
 
     public void acessarCarrinho() throws InterruptedException {
         Thread.sleep(5000);
@@ -31,5 +36,23 @@ public class PagamentoPage {
         driver.findElement(txtPostalCode).sendKeys("123456");
         Thread.sleep(5000);
         driver.findElement(btnContinue).click();
+    }
+
+    public void confirmacaoCompra(){
+        String confirmacaoProduto = driver.findElement(lblProduto).getText();
+        Assert.assertEquals("Sauce Labs Backpack", confirmacaoProduto);
+
+        String confirmacaoPagamento = driver.findElement(lblPagamento).getText();
+        Assert.assertEquals("SauceCard #31337", confirmacaoPagamento);
+
+        driver.findElement(btnFinish).click();
+    }
+
+    public void validarMensagemPagamento(String mensagemSucesso) throws InterruptedException {
+        String mensagemPagamento = driver.findElement(lblPagamentoSucesso).getText();
+        Assert.assertEquals(mensagemSucesso, mensagemPagamento);
+
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
